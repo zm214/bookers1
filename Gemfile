8c9b2ba42293aff1923baaa1c383a6468be8b491
Ubuntu <ubuntu@ip-172-31-20-236.ap-northeast-1.compute.internal>
2026-09-06 16:28:15 +0000

    [Add]レイアウト

--- Gemfile
+++ Gemfile
@@ -57,7 +57,9 @@ group :development do
 end
 
 group :test do
-  # Use system testing [https://guides.rubyonrails.org/testing.html#system-testing]
-  gem "capybara"
-  gem "selenium-webdriver"
+  gem 'capybara', '>= 2.15'
+  gem 'rspec-rails'
+  gem 'factory_bot_rails'
+  gem 'faker'
 end
+
--- config/environments/test.rb
+++ config/environments/test.rb
@@ -40,7 +40,7 @@ Rails.application.configure do
   config.action_mailer.default_url_options = { host: "example.com" }
 
   # Print deprecation notices to the stderr.
-  config.active_support.deprecation = :stderr
+  config.active_support.deprecation = :silence
 
   # Raises error for missing translations.
   # config.i18n.raise_on_missing_translations = true
